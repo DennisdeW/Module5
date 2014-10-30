902109a7868d45359e0ad3ee1fe40a3a66ca7ba2
DennisdeW <dennisdw@outlook.com>
2014-10-30 10:31:06 +0100

    Folder Monitor

--- src/main/java/files/FolderMonitor.java
+++ src/main/java/files/FolderMonitor.java
@@ -8,16 +8,34 @@ import global.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.sql.SQLException;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Observable;
+import java.util.Queue;
 
 import ssh.sftp.PiFileSystemFactory;
 import db.UserStatementMaker;
 
-public class FolderMonitor implements Runnable {
+/**
+ * Observable class which stores any changes to a certain directory in a queue
+ * for processing.
+ * 
+ * @author Dennis
+ *
+ */
+public class FolderMonitor extends Observable implements Runnable {
 
+	/**
+	 * This should be called when the program starts to put monitors on all
+	 * existing users' directories.
+	 */
 	public static void initialiseMonitors() {
 		PiFileSystemFactory.init();
 		try {
@@ -28,23 +46,100 @@ public class FolderMonitor implements Runnable {
 		}
 	}
 
-	private WatchService service;
+	/**
+	 * This should be called when a new user is added.
+	 */
+	public static void registerNewDirectory(String user) {
+		new Thread(new FolderMonitor(user)).start();
+	}
 
+	private WatchService service;
+	private Queue<Event> events;
+	private Path basePath;
+
+	/**
+	 * You should create these using the static methods above.
+	 */
 	private FolderMonitor(String user) {
-		new File(PiFileSystemFactory.homeDirs.get(user));
+		events = new ArrayDeque<>();
 		try {
+			basePath = new File(PiFileSystemFactory.homeDirs.get(user))
+					.toPath();
 			service = FileSystems.getDefault().newWatchService();
-			new File(PiFileSystemFactory.homeDirs.get(user))
-					.toPath().register(service, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE, OVERFLOW);
+			basePath.register(service, ENTRY_CREATE, ENTRY_MODIFY,
+					ENTRY_DELETE, OVERFLOW);
 		} catch (IOException e) {
 			Logger.logError("Unable to create WatchService for " + user + ":");
 			Logger.logError(e);
 		}
 	}
 
-	@Override
-	public void run() {
-
+	/**
+	 * Enqueue a new event.
+	 * 
+	 * @param e
+	 *            The new event.
+	 */
+	private synchronized void offer(Event e) {
+		events.offer(e);
 	}
 
+	/**
+	 * Get the oldest event in the queue.<br>
+	 * This should only be called by observers after they are notified.
+	 * 
+	 * @return The oldest event, or null if there aren't any.
+	 */
+	public synchronized Event poll() {
+		return events.poll();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				//take() blocks until a key is available.
+				WatchKey key = service.take();
+				List<WatchEvent<?>> events = key.pollEvents();
+				for (WatchEvent<?> ev : events) {
+					WatchEvent.Kind<?> type = ev.kind();
+
+					//Overflow means that an event was missed.
+					if (type == OVERFLOW) {
+						Logger.logError("An overflow occured in one of the FolderMonitors!");
+						continue;
+					}
+					File file = ((Path) ev.context()).toAbsolutePath().toFile();
+					offer(new Event(type, file));
+					setChanged();
+					notifyObservers();
+				}
+			} catch (InterruptedException | ClosedWatchServiceException e) {
+				Logger.logError(e);
+			}
+		}
+	}
+
+	/**
+	 * Small bean for an event.
+	 * 
+	 * @author Dennis
+	 */
+	protected static final class Event {
+		private WatchEvent.Kind<?> type;
+		private File file;
+
+		private Event(WatchEvent.Kind<?> type, File file) {
+			this.type = type;
+			this.file = file;
+		}
+
+		public WatchEvent.Kind<?> getType() {
+			return type;
+		}
+
+		public File getFile() {
+			return file;
+		}
+	}
 }
